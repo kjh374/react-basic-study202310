3e2a04be620a08ecf8097e4a6b03922ba778e991
JunHyuk <kkjjhh374@naver.com>
2023-11-07 10:17:20 +0900

    useEffect 문제 발생

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 import Card from '../Card';
 import styles from './ErrorModal.module.css';
 import Button from '../Button/Button';
+import Portal from '../Portal/Portal';
 
 const BackDrop = ({ onConfirm }) => {
   return (
@@ -35,19 +36,16 @@ const ModalOverlay = ({ title, message, onConfirm }) => {
 const ErrorModal = ({ title, message, onConfirm }) => {
   return (
     <>
-      {ReactDOM.createPortal(
+      <Portal destId='backdrop-root'>
         <BackDrop onConfirm={onConfirm} />,
-        document.getElementById('backdrop-root')
-      )}
-
-      {ReactDOM.createPortal(
+      </Portal>
+      <Portal destId='overlay-root'>
         <ModalOverlay
           title={title}
           message={message}
           onConfirm={onConfirm}
-        />,
-        document.getElementById('overlay-root')
-      )}
+        />
+      </Portal>
     </>
   );
 };
