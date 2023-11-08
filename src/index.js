6cf3df398ec45f622b3ce8bb66ee04a8f8f56ede
JunHyuk <kkjjhh374@naver.com>
2023-11-08 13:21:15 +0900

    provider 분할 및 input 컴포넌트 분할

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const overlay
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
 // overlay-root.render()
