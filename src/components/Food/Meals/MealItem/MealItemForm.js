58e23e132276237402052bc2f9be1c1c3379cd86
JunHyuk <kkjjhh374@naver.com>
2023-11-14 17:09:54 +0900

    Cart Context와 Reducer로 전역 상태 관리

--- src/components/Food/Meals/MealItem/MealItemForm.js
+++ src/components/Food/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../../UI/Input/Input';
 import styles from './MealItemForm.module.scss';
+import CartContext from '../../../../store/cart-context';
 
-const MealItemForm = (props) => {
+const MealItemForm = ({ id, onAddToCart }) => {
+  // 수량의 상태를 관리하는 변수.
+  const [amount, setAmount] = useState(0);
+
+  // 담기 버튼을 누르면 발동하는 함수.
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    onAddToCart(amount); // context에게 확정된 수량을 전달해 주자.
+  };
+
+  // 수량이 변경될 때마다 발동하는 함수.
+  const amountHandler = (amt) => {
+    setAmount(amt);
+  };
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={formSubmitHandler}
+    >
       <Input
+        onAdd={amountHandler}
         label='수량'
         input={{
-          id: 'amount_' + props.id,
+          id: 'amount_' + id,
           type: 'number',
           min: '1',
           max: '5',
@@ -16,7 +35,7 @@ const MealItemForm = (props) => {
           defaultValue: '1',
         }}
       />
-      <button>담기</button>
+      <button onClick={onAddToCart}>담기</button>
     </form>
   );
 };
---
+++ src/components/UI/Modal/CartModal.js
@@ -0,0 +1,35 @@
+import React, { Children } from 'react';
+import styles from './CartModal.module.scss';
+import Portal from '../Portal/Portal';
+
+const Backdrop = ({ onHide }) => {
+  return (
+    <div
+      className={styles.backdrop}
+      onClick={onHide}
+    />
+  );
+};
+
+const ModalOverlay = ({ children }) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+};
+
+const CartModal = ({ children, onHide }) => {
+  return (
+    <>
+      <Portal destId='backdrop-root'>
+        <Backdrop onHide={onHide} />
+      </Portal>
+      <Portal destId='overlay-root'>
+        <ModalOverlay>{children}</ModalOverlay>
+      </Portal>
+    </>
+  );
+};
+
+export default CartModal;
